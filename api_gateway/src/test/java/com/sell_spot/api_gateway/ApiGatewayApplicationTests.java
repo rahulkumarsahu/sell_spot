d316f9a3824e1d4480a074eb138e96839ca090d7
Rahul Kumar <rahulkumar060995@gmail.com>
2024-03-02 16:36:55 +0530

    Sell Spot: Project Creation

---
+++ api_gateway/src/test/java/com/sell_spot/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sell_spot.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ user_service/user_service/src/test/java/com/sell_spot/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sell_spot.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
